f5e3f169e2e2ff125a06d8cdce4d3cbaf2fe7c5e
liuhongli <liuhongli2120@gmail.com>
2024-09-30 09:14:49 +0800

    [t0]修改文件

--- SceneDelegateDisable/SceneDelegateDisable/ViewController.swift
+++ SceneDelegateDisable/SceneDelegateDisable/ViewController.swift
@@ -11,7 +11,14 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        
+        let label = UILabel.init()
+        label.text = "小熊翻译\n AppStore免费下载Mac,iOS版, 启动开挂人生"
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        self.view.addSubview(label)
+        label.frame = CGRect(x: 16, y: UIScreen.main.bounds.height/2-22, width: self.view.frame.width - 32, height: 44)
     }
 
 
